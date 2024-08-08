4485d55349e4b25acf7dd5926e834877fa07ef50
kiri23 <christian@opticpower.com>
2024-08-08 15:19:45 -0400

    add ip-packet example parsing

--- library-usage/parsing-ip-packet-header/parsingBinary.js
+++ library-usage/parsing-ip-packet-header/parsingBinary.js
@@ -25,7 +25,9 @@ import {
   updateParserError,
   updateParserState,
   sequenceOf,
-} from "../lib.js";
+} from "../../lib.js";
+import fs from "fs";
+import path from "path";
 
 const extractBit = (byte, bitIndex) => {
   // What bit we want to get: 0, 1, 2, 3, 4, 5, 6, or 7
@@ -119,6 +121,93 @@ const One = new Parser((parserState) => {
   return updateParserState(parserState, parserState.index + 1, bit);
 });
 
+const Uint = (n) => {
+  if (n < 1) {
+    throw new Error(`Uint: n must be larger than 0, but we got ${n}`);
+  }
+
+  if (n > 32) {
+    throw new Error(`Uint: n must be less than 32, but we got ${n}`);
+  }
+
+  return sequenceOf(Array.from({ length: n }, () => Bit)).map((bits) => {
+    return bits.reduce((acc, bit, i) => {
+      return acc + Number(BigInt(bit) << BigInt(n - 1 - i));
+    }, 0);
+  });
+};
+
+// Ones complement
+// SXXX
+// 0001 = 1
+// 1001 = -1
+// 0000 = 0
+// 1000 = ? -0
+
+// Twos Complement
+// 0001
+// 1110 + 0001 = 1111 -> -1
+
+// 1011
+// 0001
+//------
+// 1100
+// 0001
+// -----
+// 1101 -> -3
+
+const Int = (n) => {
+  if (n < 1) {
+    throw new Error(`Int: n must be larger than 0, but we got ${n}`);
+  }
+
+  if (n > 32) {
+    throw new Error(`Int: n must be less than 32, but we got ${n}`);
+  }
+
+  return sequenceOf(Array.from({ length: n }, () => Bit)).map((bits) => {
+    if (bits[0] === 0) {
+      return bits.reduce((acc, bit, i) => {
+        return acc + Number(BigInt(bit) << BigInt(n - 1 - i));
+      }, 0);
+    } else {
+      return -(
+        1 +
+        bits.reduce((acc, bit, i) => {
+          return acc + Number(BigInt(bit === 0 ? 1 : 0) << BigInt(n - 1 - i));
+        }, 0)
+      );
+    }
+  });
+};
+
+const RawString = (s) => {
+  if (s.length < 1) {
+    throw new Error(`RawString: s must be at least 1 character`);
+  }
+
+  const byteParsers = s
+    .split("")
+    .map((c) => c.charCodeAt(0))
+    .map((n) => {
+      return Uint(8).chain((res) => {
+        if (res === n) {
+          return succeed(n);
+        } else {
+          return fail(
+            `RawString: Expected character ${String.fromCharCode(
+              n
+            )}, but got ${String.fromCharCode(res)}`
+          );
+        }
+      });
+    });
+
+  return sequenceOf(byteParsers);
+};
+
+const binaryNumbers = [1, 255];
+// An example on how to parse bits, Number 1 (00000001), 255 (1111111)
 const parser = sequenceOf([
   Zero,
   Zero,
@@ -134,9 +223,47 @@ const parser = sequenceOf([
   Bit,
 ]);
 
-const data = new Uint8Array([1, 255]).buffer;
+const data = new Uint8Array(binaryNumbers).buffer;
 const dataView = new DataView(data);
+// const res = parser.run(dataView);
+// console.log(res);
 
-const res = parser.run(dataView);
+// Example 2 parsing binary file that contains ip header
+const tag = (type) => (value) => ({ type, value });
+const parser2 = sequenceOf([
+  Uint(4).map(tag("Version")),
+  Uint(4).map(tag("IHL")),
+  Uint(6).map(tag("DSCP")),
+  Uint(2).map(tag("ECN")),
+  Uint(16).map(tag("Total Length")),
+  Uint(16).map(tag("Identification")),
+  Uint(3).map(tag("Flags")),
+  Uint(13).map(tag("Fragment Offset")),
+  Uint(8).map(tag("TTL")),
+  Uint(8).map(tag("Protocol")),
+  Uint(16).map(tag("Header Checksum")),
+  Uint(32).map(tag("Source IP")),
+  Uint(32).map(tag("Destination IP")),
+]).chain((res) => {
+  if (res[1].value > 5) {
+    const remainingBytes = Array.from({ length: res[1].value - 20 }, () =>
+      Uint(8)
+    );
+    // In the video .chain() was used here (incorrectly)
+    // Thanks to https://github.com/IceSentry for pointing that one out
+    return sequenceOf(remainingBytes).map((remaining) => [
+      ...res,
+      tag("Options")(remaining),
+    ]);
+  }
+  return succeed(res);
+});
+
+const relativePath =
+  "/Users/christiannogueras/Documents/Personal projects/Learning /Parser-combinators/library-usage/parsing-ip-packet-header/";
+const file = fs.readFileSync(relativePath + "packet.bin").buffer;
+const dataView2 = new DataView(file);
+
+const res = parser2.run(dataView2);
 
 console.log(res);
